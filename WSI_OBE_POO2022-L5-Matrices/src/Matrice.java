e187aef78520db6319d2f63577a22f08c3a51f33
SlWa99 <walid.slimani99@gmail.com>
2022-10-13 15:13:07 +0200

    first commit

---
+++ WSI_OBE_POO2022-L5-Matrices/src/Matrice.java
@@ -0,0 +1,16 @@
+public class Matrice {
+    // region Ctor
+
+    //endregion
+
+
+    // region Paramètres privés
+    private int ligne;
+
+    private int colonne;
+
+    // endregion
+
+
+    // region Méthodes publiques
+}
